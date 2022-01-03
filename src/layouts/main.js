d35ceb940cf40097703eb497502845d5e2fda5d4
Franco-Aguero <aguerofranco2002@gmail.com>
2022-01-03 15:38:22 -0300

    add logo

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,7 +1,9 @@
 export function MainLayout({ children }) {
     return (
       <>
-        <header className="h-14 bg-indigo-600 w-full fixed top-0 z-10" />
+        <header className="flex items-center h-14 bg-indigo-600 w-full fixed top-0 z-10">
+          <h2 className="pl-32 text-gray-50 text-2xl font-semibold">SKILLIFY</h2>
+        </header>
         <main className="flex flex-col mt-14 pt-10">{children}</main>
       </>
     )
